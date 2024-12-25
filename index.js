f3b3253211d80d9a1cd21abc7fd3ba785afcd49d
dootz <gustanley312@gmail.com>
2024-12-25 18:13:58 -0500

    a

--- index.js
+++ index.js
@@ -2,8 +2,36 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// Middleware to log requests
+app.use((req, res, next) => {
+  console.log(`${req.method} request for '${req.url}'`);
+  next();
+});
+
+// Middleware to serve static files
+app.use(express.static('public'));
+
+// Routes
 app.get('/', (req, res) => {
   res.send('Hello, World!');
+});
+
+app.get('/about', (req, res) => {
+  res.send('About Us');
+});
+
+app.get('/contact', (req, res) => {
+  res.send('Contact Us');
+});
+
+app.get('/api/data', (req, res) => {
+  res.json({ message: 'This is some JSON data' });
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
 
 app.listen(port, () => {
